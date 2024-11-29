37b94038cde76b7cfb3e7821abc2afcc4581927a
podlaskialgorytm <live.m851@gmail.com>
2024-11-29 09:25:56 +0100

    feat: add function to sorting by quantity and sorting by word with swapnodes and bubble sort

---
+++ sort.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "sort.h"
+
+void swapNodes(concordancePointer a, concordancePointer b)
+{
+    char tempWord[100];
+    int tempQuantity;
+    int *tempLines;
+
+    strcpy(tempWord, a->word);
+    strcpy(a->word, b->word);
+    strcpy(b->word, tempWord);
+
+    tempQuantity = a->quantity;
+    a->quantity = b->quantity;
+    b->quantity = tempQuantity;
+
+    tempLines = a->lines;
+    a->lines = b->lines;
+    b->lines = tempLines;
+}
+
+void bubbleSortByQuantity(concordancePointer head)
+{
+    if (!head)
+        return;
+
+    int swapped;
+    concordancePointer ptr;
+    concordancePointer lptr = NULL;
+
+    do
+    {
+        swapped = 0;
+        ptr = head;
+
+        while (ptr->next != lptr)
+        {
+            if (ptr->quantity > ptr->next->quantity)
+            {
+                swapNodes(ptr, ptr->next);
+                swapped = 1;
+            }
+            ptr = ptr->next;
+        }
+        lptr = ptr;
+    } while (swapped);
+}
+
+void bubbleSortByWord(concordancePointer head)
+{
+    if (!head)
+        return;
+
+    int swapped;
+    concordancePointer ptr;
+    concordancePointer lptr = NULL; // Ostatni posortowany element
+
+    do
+    {
+        swapped = 0;
+        ptr = head;
+
+        while (ptr->next != lptr)
+        {
+            if (strcmp(ptr->word, ptr->next->word) > 0)
+            { // Porównanie alfabetyczne
+                swapNodes(ptr, ptr->next);
+                swapped = 1;
+            }
+            ptr = ptr->next;
+        }
+        lptr = ptr; // Zmniejszamy zakres sortowania
+    } while (swapped);
+}
---
+++ sort.h
@@ -0,0 +1,10 @@
+#ifndef _SORT_H
+#include "list.h"
+#define _SORT_H
+
+void swapNodes(concordancePointer a, concordancePointer b);
+
+void bubbleSortByQuantity(concordancePointer head);
+void bubbleSortByWord(concordancePointer head);
+
+#endif
