249d4938c09924174b0a7ef365a69a82917f1241
podlaskialgorytm <live.m851@gmail.com>
2024-11-30 21:41:51 +0100

    feat: update main func

--- main.c
+++ main.c
@@ -2,30 +2,61 @@
 #include <stdlib.h>
 #include "list.h"
 #include "sort.h"
+#include "search.h"
+
+#define MAX 1024
 
 int main(int argc, char **argv)
 {
+    char *fileName = argc > 1 ? argv[1] : "";
+    int sortMode = argc > 2 ? atoi(argv[2]) : -1;
+    int n = argc - 3;
+    char **words = malloc(n * sizeof(char *));
+
+    for (int i = 0; i < n; i++)
+    {
+        *(words + i) = argv[i + 3];
+    }
+
+    if (fileName == "")
+    {
+        printf("File name is not written.\n");
+        return 1;
+    }
+
+    if (!(sortMode == 1 || sortMode == 0))
+    {
+        printf("Sort mode is not entered.\n");
+        return 1;
+    }
+
+    if (argc < 4)
+    {
+        printf("Any words are not written.\n");
+        return 1;
+    }
+
     concordancePointer wordList = NULL;
 
-    wordList = insert(wordList, "mozg", 3, (int[]){1, 2, 3});
-    wordList = insert(wordList, "nogi", 4, (int[]){1, 2, 3, 9});
-    wordList = insert(wordList, "glowa", 5, (int[]){1, 2, 3, 44, 55});
-    wordList = insert(wordList, "czolo", 6, (int[]){1, 2, 3, 55, 33, 22});
-    wordList = insert(wordList, "reka", 2, (int[]){2, 3});
+    for (int i = 0; i < n; i++)
+    {
+        int *lines = get_lines(fileName, *(words + i));
+        int number_of_lines = count_word_in_file(fileName, *(words + i));
+        wordList = insert(wordList, *(words + i), number_of_lines, lines);
+    }
 
-    printStruct(wordList);
-
-    bubbleSortByQuantity(wordList);
-
-    printf("Po sortowaniu po liczbie: \n");
-
-    printStruct(wordList);
-
-    printf("Po sortowaniu po slowie: \n");
-
-    bubbleSortByWord(wordList);
-
-    printStruct(wordList);
+    if (sortMode == 0)
+    {
+        printf("Sorting by word: \n");
+        bubbleSortByWord(wordList);
+        printStruct(wordList);
+    }
+    else if (sortMode == 1)
+    {
+        printf("Sorting by quantity: \n");
+        bubbleSortByQuantity(wordList);
+        printStruct(wordList);
+    }
 
     return 0;
 }
