df13d36e29f30e9ebfab308853367ca00ab1e4d2
podlaskialgorytm <live.m851@gmail.com>
2024-11-30 21:41:03 +0100

    improve struct

--- list.h
+++ list.h
@@ -1,16 +1,16 @@
 #ifndef _LIST_H
 #define _LIST_H
 
-typedef struct concordance {
+typedef struct concordance
+{
     char word[100];
     int quantity;
     int *lines;
     struct concordance *next;
-}concordance, *concordancePointer;
+} concordance, *concordancePointer;
 
 concordancePointer insert(concordancePointer list, char *word, int quantity, int *lines);
 
 void printStruct(concordancePointer wordList);
 
-#endif 
-
+#endif
